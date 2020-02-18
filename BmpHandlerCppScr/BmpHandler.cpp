2a1855a1ef9f4abe1b9b463ccbbef4a6b4424f4b
Ilya <villeman.5@yandex.ru>
2020-02-18 15:34:24 +0500

    fixed padding in bmp files

--- BmpHandlerCppScr/BmpHandler.cpp
+++ BmpHandlerCppScr/BmpHandler.cpp
@@ -1,4 +1,4 @@
-#pragma pack(2)
+#pragma pack(push, 2)
 #include <iostream>
 #include <fstream>
 
@@ -20,7 +20,7 @@ struct bmpInfoHeader
 	//12 + 14 = 26 bytes
 
 	uint16_t planes;
-	uint16_t bpp; //1, 4, 8, 16, 24, (32, 64) bit per pixel
+	uint16_t bpp; //1, 4, 8, 16, 24, 32 bit per pixel
 	uint32_t compression;
 	uint32_t bit_size_image;
 	int32_t x_ppm;
@@ -29,21 +29,6 @@ struct bmpInfoHeader
 	uint32_t colors_important;
 	//26 + 28 = 54 bytes
 };
-
-//struct RGBQUAD
-//{
-//	uint8_t blue;
-//	uint8_t green;
-//	uint8_t red;
-//	uint8_t reserved;
-//};
-//
-//struct RGBTRIPLE
-//{
-//	uint8_t blue;
-//	uint8_t green;
-//	uint8_t red;
-//};
 
 struct bmpFile
 {
@@ -66,22 +51,27 @@ struct bmpFile
 			}
 			inFile.read((char*)&infoHeader, sizeof(bmpInfoHeader));
 			inFile.seekg(header.offset);
-			if (infoHeader.colors_used > 0)
+			if (infoHeader.bpp <= 8)
 			{
 				palette = new uint8_t[4 * infoHeader.colors_used];
 				for (int i = 0; i < 4 * infoHeader.colors_used; i++)
 					inFile.read((char*)palette[i], sizeof(char));
 			}
 			int height = abs(infoHeader.height);
-			int bitwidth = infoHeader.width * infoHeader.bpp / 8;
+			int width = infoHeader.width * infoHeader.bpp / 8;
+			int padding = 4 - (width % 4);
+			width += padding;
 
 			data = new uint8_t*[height];
 			for (int i = 0; i < height; i++)
-				data[i] = new uint8_t[bitwidth];
+				data[i] = new uint8_t[width];
 
-			for (int i = 0; i < height; i++) 
-				for(int j = 0; j < bitwidth; j++)
+			for (int i = 0; i < height; i++)
+			{
+				for (int j = 0; j < width; j++)
 					inFile.read((char*)&data[i][j], 1);
+			}
+
 			inFile.close();
 			return true;
 		}
@@ -98,20 +88,27 @@ struct bmpFile
 			outFile.write(reinterpret_cast<char*>(&header), sizeof(header));
 			outFile.write(reinterpret_cast<char*>(&infoHeader), sizeof(infoHeader));
 			outFile.seekp(header.offset);
-			if (infoHeader.colors_used != 0)
+			if (infoHeader.bpp <= 8)
 			{
 				palette = new uint8_t[4 * infoHeader.colors_used];
 				for (int i = 0; i > 4 * infoHeader.colors_used; i++)
 					outFile.write((char*)palette[i], sizeof(char));
 			}
 			int height = abs(infoHeader.height);
-			int bitwidth = infoHeader.width * infoHeader.bpp / 8;
+			int width = infoHeader.width * infoHeader.bpp / 8;
+			int padding = 4 - (width % 4);
+			width += padding;
 
 			for (int i = 0; i < height; i++)
-				for (int j = 0; j < bitwidth; j++)
+			{
+				for (int j = 0; j < width; j++)
+				{
 					outFile.write((char*)&data[i][j], 1);
+				}
+			}
 		}
 		else
 			return false;
 	}
 };
+#pragma pack (pop)
