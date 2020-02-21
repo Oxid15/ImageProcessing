0aae8f67bafae618abc5e29144927edd42d22ae5
Ilya <villeman.5@yandex.ru>
2020-02-21 15:57:30 +0500

    added base for filtering

--- FilteringCppSrc/filtering.cpp
+++ FilteringCppSrc/filtering.cpp
@@ -24,29 +24,33 @@ public:
 		kernelY = _kernelY;
 	}
 
-	void operator()(T** src, T** out, uint32_t height, uint32_t width, int i, int j)
+	void operator()(T*** src, T*** out, uint32_t height, uint32_t width, uint8_t colors, int i, int j)
 	{
-		int ki = 0;
-		int kj = 0;
-		int up = i - floor(kernelY / 2);
-		int left = j - floor(kernelX / 2);
-		int down = i + floor(kernelY / 2) + 1;
-		int right = j + floor(kernelX / 2) + 1;
-		uint64_t sumWeights = 0;
-		uint64_t result = 0;
-		for (int k = up; k < down; k++)
+		//for each color plane do the filtering
+		for (uint8_t c = 0; c < colors; c++)
 		{
-			if (k < 0 || k >= height) continue;
-			for (int l = left; l < right; l++)
+			int up = i - floor(kernelY / 2);//TODO: check conversion to double
+			int left = j - floor(kernelX / 2);
+			int down = i + floor(kernelY / 2) + 1;
+			int right = j + floor(kernelX / 2) + 1;
+			int64_t sumWeights = 0;
+			int64_t result = 0;
+			int ki = 0;
+			for (int k = up; k < down; k++)
 			{
-				if (l < 0 || l >= width) continue;
-				result += kernel[ki][kj] * src[k][l]; //TODO: check how type conversion works
-				sumWeights += kernel[ki][kj];
-				kj++;
+				if (k < 0 || k >= height) continue;
+				int kj = 0;
+				for (int l = left; l < right; l++)
+				{
+					if (l < 0 || l >= width) continue;
+					result += kernel[ki][kj] * src[k][l][c];
+					sumWeights += kernel[ki][kj];
+					kj++;
+				}
+				ki++;
 			}
-			ki++;
+			out[i][j][c] = T(round(result / sumWeights)); //same here
 		}
-		out[i][j] = T(round(result / sumWeights)); //same here
 	}
 };
 
@@ -54,7 +58,7 @@ public:
 //class OrderStatKernel :Kernel
 //{
 //public:
-//	T** operator()(T** neighb)
+//	T** operator()(T** )
 //	{
 //
 //	}
@@ -71,14 +75,10 @@ public:
 		kernel = _kernel;
 	}
 
-	void operator()(T** src, T** out, uint32_t height, uint32_t width)
+	void operator()(T*** src, T*** out, uint32_t height, uint32_t width, uint8_t colors)
 	{
 		for (uint32_t i = 0; i < height; i++)
-		{
 			for (uint32_t j = 0; j < width; j++)
-			{
-				kernel(src, out, height, width, i, j);
-			}
-		}
+				kernel(src, out, height, width, colors, i, j);
 	}
 };
